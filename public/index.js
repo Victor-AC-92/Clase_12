a357797d02c18d99ab761391bdf455af3180aa9e
Victor Antuña <victorac92@hotmail.com>
2023-01-03 19:40:58 -0300

    Primer commit

---
+++ public/index.js
@@ -0,0 +1,33 @@
+let socket = io.connect()
+
+socket.on('messages', data => {
+    console.log(data)
+    renderizar(data)
+});
+
+function renderizar(data) {
+    let html = data.map((elem, index) => {
+        return (`<div>
+        <strong style="color: blue">${elem.author}</strong>
+        <em style="color: green">${elem.text}</em>
+        </div>
+        `)
+    }).join(" ");
+    document.getElementById('messages').innerHTML = html
+}
+
+function addMessage(e) {
+    let message = {
+        author: document.getElementById('email').value,
+        text: document.getElementById('texto').value
+    }
+
+    socket.emit("new-message", message)
+
+    document.getElementById('email').value = ""
+    document.getElementById('texto').value = ""
+
+    return false;
+}
+
+socket.on('messages', data => {renderizar(data);});
---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const {engine} = require('express-handlebars')
+
+
+class Producto {
+    constructor (nombre, precio, url){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.url = url;
+    }
+}
+
+let productos = []
+let messages = [
+    {author: "Juan", text: "Hola!"}
+]
+
+//Configuracion Handlebars
+app.engine('handlebars', engine())
+app.use(express.urlencoded({extended: true}))
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
+
+app.use(express.static('public'))
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    console.log(productos);
+    res.redirect('/')
+})
+
+app.get('/', (req, res) => {
+    res.render('formulario', {productos})
+})
+
+io.on('connection', socket => {
+    console.log('Un cliente se ha conectado');
+    socket.emit('messages', messages);
+
+    socket.on('new-message', data => {
+        messages.push(data);
+        io.sockets.emit('messages', messages);
+    });
+});
+
+const PORT = 8080
+
+const srv = server.listen(PORT, () => console.log(`Servidor http con WebSocket escuchando el puerto ${srv.address().port}`))
+srv.on('error', error => console.log(`Error en el servidor ${error}`))
